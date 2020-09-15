e6e6bb0d20ad6dbab8645d2b27ce99b9257bc441
htr-volker <htr.volker@gmail.com>
2020-09-15 18:01:04 +0100

    database fix 8

--- backend/create.py
+++ backend/create.py
@@ -7,17 +7,19 @@ db.create_all()
 
 trainer1 = Trainers(first_name="Bill S.", last_name="Preston, Esq.")
 trainer2 = Trainers(first_name="Ted 'Theodore'", last_name="Logan")
+trainers = [trainer1, trainer2]
+db.session.add_all(trainers)
+db.session.commit()
 
 intake = Intakes(name="SeptDevOps", start_date=date(2020, 9, 14))
-intake.trainers.append(trainer1)
-intake.trainers.append(trainer2)
+intake.trainers.append(Trainers.query.filter_by(first_name="Bill S.").first())
+intake.trainers.append(Trainers.query.filter_by(first_name="Ted 'Theodore'").first())
+db.session.add(intake)
+db.session.commit()
 
-trainee1 = Trainees(first_name="Thea", last_name="Preston", intake_id=intake.id)
-trainee2 = Trainees(first_name="Billie",last_name="Logan", intake_id=intake.id)
 
-objects = [
-    trainer1, trainer2, trainee1, trainee2, intake
-]
-
-db.session.add_all(objects)
+trainee1 = Trainees(first_name="Thea", last_name="Preston", intake_id=Intakes.query.filter_by(name="SeptDevOps").id)
+trainee2 = Trainees(first_name="Billie",last_name="Logan", intake_id=Intakes.query.filter_by(name="SeptDevOps").id)
+trainees = [trainee1, trainee2]
+db.session.add_all(traineers)
 db.session.commit()
