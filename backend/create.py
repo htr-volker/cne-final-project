914d5495f3c9cc516a374d0936970fbac8ba5db5
htr-volker <htr.volker@gmail.com>
2020-09-15 16:38:19 +0100

    database fix 3

--- backend/create.py
+++ backend/create.py
@@ -8,12 +8,12 @@ db.create_all()
 trainer1 = Trainers(first_name="Bill S.", last_name="Preston, Esq.")
 trainer2 = Trainers(first_name="Ted 'Theodore'", last_name="Logan")
 
-trainee1 = Trainees(first_name="Thea", last_name="Preston")
-trainee2 = Trainees(first_name="Billie",last_name="Logan")
-
 intake = Intakes(name="SeptDevOps", start_date=date(2020, 9, 14))
 intake.trainers.append(trainer1)
 intake.trainers.append(trainer2)
+
+trainee1 = Trainees(first_name="Thea", last_name="Preston", intake_id=intake.id)
+trainee2 = Trainees(first_name="Billie",last_name="Logan", intake_id=intake.id)
 
 objects = [
     trainer1, trainer2, trainee1, trainee2, intake
