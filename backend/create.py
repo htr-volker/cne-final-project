f4644ecf4326b96aba2e8e4b85c81b0e111b2387
htr-volker <htr.volker@gmail.com>
2020-09-15 16:08:48 +0100

    fixed syntax error

--- backend/create.py
+++ backend/create.py
@@ -6,14 +6,14 @@ db.drop_all()
 db.create_all()
 
 objects = [
-    trainer1 = Trainers(first_name="Bill S.", last_name="Preston, Esq.")
-    trainer2 = Trainers(first_name="Ted 'Theodore'", last_name="Logan")
+    trainer1 = Trainers(first_name="Bill S.", last_name="Preston, Esq."),
+    trainer2 = Trainers(first_name="Ted 'Theodore'", last_name="Logan"),
 
-    trainee1 = Trainees(first_name="Thea", last_name="Preston")
-    trainee2 = Trainees(first_name="Billie",last_name="Logan")
+    trainee1 = Trainees(first_name="Thea", last_name="Preston"),
+    trainee2 = Trainees(first_name="Billie",last_name="Logan"),
 
-    intake = Intake(name="SeptDevOps", start_date=date(2020, 9, 14))
-    intake.trainers.append(trainer1)
+    intake = Intake(name="SeptDevOps", start_date=date(2020, 9, 14)),
+    intake.trainers.append(trainer1),
     intake.trainers.append(trainer2)
 ]
 
